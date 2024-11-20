743057e9e0d3af44c0ca6460754730718d677015
Francisco Cea <francisco.cea@usach.cl>
2024-11-20 14:37:24 -0300

    Microservicio PBRequest

---
+++ PBRequest/src/main/java/org/example/PBRequest/RequestServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example.PBRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestServiceApplication.class, args);
+    }
+}
---
+++ PBRequest/src/main/java/org/example/PBRequest/entity/Request.java
@@ -0,0 +1,27 @@
+package org.example.PBRequest.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "requests")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String rut; //Rut Cliente que solicita el préstamo
+    private String type; //Tipo de préstamo
+    private int amount; //Monto préstamo
+    private float interest; //Tasa de interés anual
+    private int time; //Plazo en años
+    private int state; //Estado solicitud
+}
